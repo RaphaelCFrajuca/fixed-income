e2f4373843898c2ca649d91ebac06ceeb4415fe1
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2025-02-17 00:16:28 -0300

    Removido lixo, ajuste do modulo main e adicionadas algumas dependencias obrigatórias.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import "reflect-metadata";
+import { FixedIncomeModule } from "./fixed-income.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(FixedIncomeModule);
+    app.enableCors();
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            transformOptions: { enableImplicitConversion: true },
+            forbidNonWhitelisted: true,
+        }),
+    );
+    await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
