90972d10601fb0b77fb7b294b8c699fd4421080f
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2025-02-17 16:29:58 -0300

    criada estrutura base do modulo de produto

--- src/domain/product/product.module.ts
+++ src/domain/product/product.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { DatabaseModule } from "src/common/database/database.module";
+import { ProductController } from "./controllers/product.controller";
+import { ProductService } from "./services/product.service";
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
+    imports: [DatabaseModule],
+    controllers: [ProductController],
+    providers: [ProductService],
 })
 export class ProductModule {}
