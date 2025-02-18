11573824cc3f2d0f7e4755a08fabde450fdf700d
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2025-02-18 16:03:42 -0300

    correção setup inicial de testes

--- test/client/unit/client-service.unit.spec.ts
+++ test/client/unit/client-service.unit.spec.ts
@@ -1,5 +1,6 @@
 import { Test } from "@nestjs/testing";
 import { Database } from "src/common/database/interfaces/database.interface";
+import { Client } from "src/domain/client/entities/client.entity";
 import { ClientService } from "src/domain/client/services/client.service";
 
 describe("ClientService (unit)", () => {
@@ -28,5 +29,20 @@ describe("ClientService (unit)", () => {
 
     it("should be defined", () => {
         expect(service).toBeDefined();
+    });
+
+    describe("create", () => {
+        it("should create a client when valid data is provided", async () => {
+            const client: Client = {
+                name: "John Doe",
+                documentNumber: "12345678900",
+                address: "Rua dos Bobos, nº 0",
+                annualIncome: 1000,
+            };
+            const dbSpy = jest.spyOn(mockDatabase, "create").mockResolvedValue(null);
+
+            expect(await service.create(client)).toBeNull();
+            expect(dbSpy).toHaveBeenCalledWith(client);
+        });
     });
 });
